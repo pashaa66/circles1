6ab3325aa0e25edc318aba9538bb8f408da70975
Pavel <pasha10428@gmail.com>
2023-11-23 21:32:01 +0300

    second

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 
 from PyQt5 import uic, QtCore
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from random import randint
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -13,12 +15,33 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
 class MyProgram(QMainWindow):
     def __init__(self):
         super().__init__()
-        # uic.loadUi('ui.ui', self) # from ui file
-        # self.setupUi(self) # from py file (need to import class from generated .py file)
+        uic.loadUi('UI.ui', self)
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
         self.initUI()
+        print(self.height())
 
     def initUI(self):
         pass
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        x = randint(0, self.width())
+        y = randint(0, self.height())
+        d = randint(0, 600)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, d, d)
 
 
 def except_hook(cls, exception, traceback):
@@ -31,4 +54,3 @@ if __name__ == '__main__':
     form.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
-
